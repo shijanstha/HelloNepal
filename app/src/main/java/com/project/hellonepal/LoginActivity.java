df353e7391619267b8468a70b0d7fd0a408cbe97
Shijan Shrestha <shijan0408@gmail.com>
2017-07-31 20:40:35 +0545

    explore button added

--- app/src/main/java/com/project/hellonepal/LoginActivity.java
+++ app/src/main/java/com/project/hellonepal/LoginActivity.java
@@ -51,6 +51,13 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
+
+        findViewById(R.id.explore).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            }
+        });
     }
 
 
